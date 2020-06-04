62b9f305c77ca1b41172a7fc9c5df2043595d8a1
Jason Kao <jason.kao@columbia.edu>
2020-06-03 23:23:13 -0400

    regl triangle example

--- main.js
+++ main.js
@@ -1,87 +1,68 @@
-/**
- * Vertex shader receives vertex position from attribute aVertexPosition.
- * We provide uModelViewMatrix and uProjectionMatrix.
- * We set gl_Position as a result.
- */
-const vsSource = glsl`
-  attribute vec4 aVertexPosition;
+import reglFn from 'regl';
 
-  uniform mat4 uModelViewMatrix;
-  uniform mat4 uProjectionMatrix;
+const regl = reglFn();
 
-  void main() {
-    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-  }
-`;
+regl.clear({
+  color: [0, 0, 0, 1],
+  depth: 1
+})
 
-/**
- * Fragment shader is called once for every pixel on each shape to be drawn,
- * after the shape's vertices have been processed by the vertex shader.
- */
-const fsSource = glsl`
-  void main() {
-    gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
-  }
-`;
+const drawTriangle = regl({
+  // Shaders in regl are just strings.  You can use glslify or whatever you want
+  // to define them.  No need to manually create shader objects.
+  frag: glsl`
+    precision mediump float;
+    uniform vec4 color;
+    void main() {
+      gl_FragColor = color;
+    }`,
 
-function main() {
-  const canvas = document.querySelector('#glCanvas');
-  const gl = canvas.getContext('webgl'); // Initialize the GL context
+  vert: glsl`
+    precision mediump float;
+    attribute vec2 position;
+    void main() {
+      gl_Position = vec4(position, 0, 1);
+    }`,
 
-  if (gl === null) {
-    return console.error('Unable to initialize WebGL.');
-  }
+  // Here we define the vertex attributes for the above shader
+  attributes: {
+    // regl.buffer creates a new array buffer object
+    position: regl.buffer([
+      [-2, -2], // no need to flatten nested arrays, regl automatically
+      [4, -2], // unrolls them into a typedarray (default Float32)
+      [4, 4],
+    ]),
+    // regl automatically infers sane defaults for the vertex attribute pointers
+  },
 
-  // Set clear color to black, fully opaque
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  // Clear the color buffer with specified clear color
-  gl.clear(gl.COLOR_BUFFER_BIT);
-}
+  uniforms: {
+    // This defines the color of the triangle to be a dynamic variable
+    color: regl.prop('color'),
+  },
 
-window.onload = main;
+  // This tells regl the number of vertices to draw in this command
+  count: 3,
+});
+
+// regl.frame() wraps requestAnimationFrame and also handles viewport changes
+regl.frame(({ time }) => {
+  // clear contents of the drawing buffer
+  regl.clear({
+    color: [0, 0, 0, 0],
+    depth: 1,
+  });
+
+  // draw a triangle using the command defined above
+  drawTriangle({
+    color: [
+      Math.cos(time * 0.1),
+      Math.sin(time * 0.8),
+      Math.cos(time * 0.3),
+      1,
+    ],
+  });
+});
 
 function glsl(x) {
   return x;
-}
-
-/**
- * Initialize a shader program, so WebGL knows how to draw our data
- */
-function initShaderProgram(gl, vsSource, fsSource) {
-  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
-
-  // Create the shader program and attach the shaders
-
-  const shaderProgram = gl.createProgram();
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
-
-  // Check if program successfully made
-  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    return console.error(
-      'Unable to initialize shader program:',
-      gl.getProgramInfoLog(shaderProgram),
-    );
-  }
-
-  return shaderProgram;
-}
-
-/**
- * Creates a shader of the given type, uploads the source and compiles it
- */
-function loadShader(gl, type, source) {
-  const shader = gl.createShader(type); // Make a new shader
-
-  gl.shaderSource(shader, source); // Send the source code to the shader object
-  gl.compileShader(shader); // Compile the shader
-
-  // Check if the shader was successfully compiled
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    console.error('Shader compilation error:', gl.getShaderInfoLog(shader));
-    return gl.deleteShader(shader);
-  }
-  return shader;
 }
